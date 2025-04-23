dd41cc2bde69cfee5b48564838b2dfbcf3d5b4d9
JeroeN <jcaniemendal@gmail.com>
2025-04-23 13:18:50 +0200

    added ViT

--- ocr/inference.py
+++ ocr/inference.py
@@ -12,8 +12,14 @@ image_tensor = transformer(image)
 image_tensor = image_tensor.unsqueeze(0)
 print(image_tensor.shape)
 patch_size = 16
-embeding_dimension = 64
-patch_embeding_model = ocr_model.PatchEmbeding(image_tensor.shape[2], image_tensor.shape[1], patch_size, embeding_dimension)
-output = patch_embeding_model(image_tensor)
+embeding_dimension = 64 #768 base
+depth = 12 #12 base
+num_heads = 4 #12 base
+num_classes = 27
+
+#patch_embeding_model = ocr_model.PatchEmbeding(image_tensor.shape[2], image_tensor.shape[1], patch_size, embeding_dimension)
+#output = patch_embeding_model(image_tensor)
+model = ocr_model.ViT(image_tensor.shape[2], image_tensor.shape[1], patch_size, embeding_dimension, num_heads, depth, num_classes)
+output = model(image_tensor)
 print(output)
 print(output.shape)
--- ocr/ocr_model.py
+++ ocr/ocr_model.py
@@ -1,20 +1,20 @@
 import torch
 import torch.nn as nn
 
-class PatchEmbeding(nn.Module):
-  def __init__(self, image_width, image_height, patch_size, embeding_dimension):
-    super(PatchEmbeding, self).__init__()
+class PatchEmbedding(nn.Module):
+  def __init__(self, image_width, image_height, patch_size, embedding_dimension):
+    super(PatchEmbedding, self).__init__()
 
     self.patch_size = patch_size
-    self.embeding_dimension = embeding_dimension
+    self.embedding_dimension = embedding_dimension
 
     self.num_patches_width = (image_width // patch_size)
     self.num_patches_height = (image_height // patch_size)
-
-    self.linear = nn.Linear(patch_size * patch_size, embeding_dimension)
-
-  def forward(self, images):
-    #flattened_patches = self.get_flattened_patches(images)
+    self.total_num_patches = self.num_patches_width * self.num_patches_height
+    
+    self.linear = nn.Linear(patch_size * patch_size, embedding_dimension)
+     
+  def linear_projection(self, images): 
     # Unfold the image to get patches of shape [batch_size, num_patches, patch_size*patch_size]
     patches = images.unfold(2, self.patch_size, self.patch_size).unfold(3, self.patch_size, self.patch_size)
     print(patches.shape)
@@ -25,7 +25,43 @@ class PatchEmbeding(nn.Module):
     print(patches.shape)
     patch_embeddings = self.linear(patches)
     return patch_embeddings
+    
+  def forward(self, images):
+    patch_embeddings = self.linear_projection(images)
+    return patch_embeddings
 
 
-
-
+class ViT(nn.Module):
+  def __init__(self, image_width, image_height, patch_size, embedding_dimension, num_heads, depth, num_classes):
+    super(ViT, self).__init__()
+    
+    self.patch_embedder = PatchEmbedding(image_width, image_height, patch_size, embedding_dimension)
+    
+    self.cls_token = nn.Parameter(torch.randn(1, 1, embedding_dimension))
+    self.position_embedding = nn.Parameter(torch.randn(1, 1 + self.patch_embedder.total_num_patches, embedding_dimension))
+    
+    self.encoder = nn.TransformerEncoder(
+      nn.TransformerEncoderLayer(
+        d_model =  embedding_dimension,
+        nhead = num_heads,
+        dim_feedforward = embedding_dimension * 4,
+        batch_first = True,
+      ),
+      num_layers=depth
+    )
+    
+    self.mlp_head = nn.Sequential(
+      nn.LayerNorm(embedding_dimension),
+      nn.Linear(embedding_dimension, num_classes)
+    )
+    
+  def forward(self, images):
+    patch_embeddings = self.patch_embedder(images)
+    cls_token = self.cls_token.expand(images.size(0), -1, -1)
+    patch_embeddings = torch.cat([cls_token, patch_embeddings], dim=1)
+    patch_embeddings = patch_embeddings + self.position_embedding
+    
+    visual_features = self.encoder(patch_embeddings)
+    
+    cls_output = visual_features[:, 0]
+    return self.mlp_head(cls_output)
