7170a0701648d517e60366baf440d8cc1169d813
Tovaris <30593536+viktorvesely@users.noreply.github.com>
2025-05-09 16:39:46 +0200

    segtrain

---
+++ segmentation/segtrain.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --mem=16GB
+#SBATCH --time=4:00:00
+#SBATCH --job-name=lstrain
+#SBATCH --partition=gpu
+#SBATCH --gpus-per-node=v100:1
+#SBATCH --nodes=1
+
+module purge
+module load PyTorch-bundle/2.1.2-foss-2023a-CUDA-12.1.1
+
+source ../linesegmentation/sea/bin/activate
+srun python train.py
