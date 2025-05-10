7dcf48dcbc40ee28da2a89709b43c540c9a61ec9
Tovaris <30593536+viktorvesely@users.noreply.github.com>
2025-05-10 15:17:25 +0200

    PICE finally

--- segmentation/unet.py
+++ segmentation/unet.py
@@ -5,37 +5,69 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class UNet(nn.Module):
-    def __init__(self, num_classes: int, base_ch: int = 20):
+    def __init__(self, num_classes: int, base_ch: int = 30):
         super().__init__()
         self.num_classes = num_classes
         self.base_ch = base_ch
-        self.enc1 = self.conv_block(1, base_ch, False)
-        self.enc2 = self.conv_block(base_ch, base_ch * 2, True)
-        self.enc3 = self.conv_block(base_ch * 2, base_ch * 4, True)
-        self.enc4 = self.conv_block(base_ch * 4, base_ch * 6, True)
+
+        self.enc1 = self.conv_block(1, base_ch)
+        self.enc2 = self.conv_block(base_ch, base_ch * 2)
+        self.enc3 = self.conv_block(base_ch * 2, base_ch * 4)
+        self.enc4 = self.conv_block(base_ch * 4, base_ch * 8)
+
         self.bottleneck = nn.Sequential(
-            nn.Conv2d(base_ch * 6, base_ch * 6, kernel_size=5, padding=2),
-            nn.LeakyReLU(),
+            nn.Conv2d(base_ch * 8, base_ch * 8, kernel_size=3, padding=1),
+            nn.LeakyReLU()
         )
-        self.dec4 = self.dec_conv_block(base_ch * 6, base_ch * 4)
-        self.dec3 = self.dec_conv_block(base_ch * 4, base_ch * 2)
-        self.dec2 = self.dec_conv_block(base_ch * 2, num_classes)
-        self.dec1 = self.dec_conv_block(num_classes, num_classes, actual_in=num_classes + base_ch)
-        self.final = nn.Conv2d(num_classes, num_classes, kernel_size=1)
+
+        self.dec4 = self.dec_conv_block(base_ch * 8 * 2, base_ch * 4)
+        self.dec3 = self.dec_conv_block(base_ch * 4 * 2, base_ch * 2)
+        self.dec2 = self.dec_conv_block(base_ch * 2 * 2, base_ch)
+        self.dec1 = self.dec_conv_block(base_ch * 2, base_ch)
+
+        self.final = nn.Conv2d(base_ch, num_classes + 1, kernel_size=1)
+
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    def conv_block(self, in_ch, out_ch, down: bool):
-        stride = 2 if down else 1
+    def forward(self, x):
+
+        e1 = self.enc1(x)
+        e2 = self.enc2(e1)
+        e3 = self.enc3(e2)
+        e4 = self.enc4(e3)
+
+        b = self.bottleneck(e4)
+
+        in_d4 = torch.cat((e4, b), dim=1)
+        d4 = self.dec4(in_d4)
+        d4 = self._pad(d4, e3)
+
+        in_d3 = torch.cat((e3, d4), dim=1)
+        d3 = self.dec3(in_d3)
+        d3 = self._pad(d3, e2)
+
+        in_d2 = torch.cat((e2, d3), dim=1)
+        d2 = self.dec2(in_d2)
+        d2 = self._pad(d2, e1)
+
+        in_d1 = torch.cat((e1, d2), dim=1)
+        d1 = self.dec1(in_d1)
+        d1 = self._pad(d1, x)
+
+        return self.final(d1)
+
+    def conv_block(self, in_ch, out_ch):
+
         return nn.Sequential(
-            nn.Conv2d(in_ch, out_ch, 5, padding=2, stride=stride),
-            nn.LeakyReLU(inplace=True),
+            nn.Conv2d(in_ch, out_ch, kernel_size=5, padding=2, stride=2),
+            nn.LeakyReLU(),
         )
 
-    def dec_conv_block(self, in_ch: int, out_ch: int, actual_in: int = None) -> nn.Sequential:
-        if actual_in is None:
-            actual_in = in_ch * 2
+    def dec_conv_block(self, in_ch: int, out_ch: int) -> nn.Sequential:
+
         return nn.Sequential(
-            nn.ConvTranspose2d(actual_in, out_ch, kernel_size=2, stride=2),
+            nn.ConvTranspose2d(in_ch, out_ch, kernel_size=4, stride=2, padding=1),
+            nn.Conv2d(out_ch, out_ch, kernel_size=5, padding=2),
             nn.LeakyReLU(),
         )
 
@@ -46,27 +78,6 @@ class UNet(nn.Module):
         assert diff_y < 5 and diff_x < 5, f"{diff_y}, {diff_x}"
 
         return F.pad(x, (diff_x // 2, diff_x - diff_x // 2, diff_y // 2, diff_y - diff_y // 2))
-
-    def forward(self, x):
-        e1 = self.enc1(x)
-        e2 = self.enc2(e1)
-        e3 = self.enc3(e2)
-        e4 = self.enc4(e3)
-        b = self.bottleneck(e4)
-        d4 = torch.cat((e4, b), dim=1)
-        d4 = self.dec4(d4)
-        d4 = self._pad(d4, e3)
-        d3 = torch.cat((e3, d4), dim=1)
-        d3 = self.dec3(d3)
-        d3 = self._pad(d3, e2)
-        d2 = torch.cat((e2, d3), dim=1)
-        d2 = self.dec2(d2)
-        d2 = self._pad(d2, e1)
-        d1 = torch.cat((e1, d2), dim=1)
-        d1 = self.dec1(d1)
-        d1 = self._pad(d1, x)
-        return self.final(d1)
-
 
     def save(self, folder: Path, name: str = "unet.pt", optimizer: torch.optim.Optimizer | None = None):
 
